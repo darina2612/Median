442b7f23e2d34934e808a82a3f58dffbe36583bd
Darina Krasteva <darina.krasteva@egt-bg.com>
2019-08-12 10:40:45 +0300

    Median structure with better complexity implemented.

--- Median.h
+++ Median.h
@@ -1,14 +1,15 @@
 #pragma once
 #include <queue>
 
-template <typename ValueType,
-          typename = std::enable_if_t<std::is_arithmetic<ValueType>::value>>
+template <typename ValueType>
 class Median
 {
+    static_assert (std::is_arithmetic<ValueType>::value, "Template type must be arithmetic.");
+
 public:
     Median() = default;
 
-    void add(ValueType entry);
+    void add(const ValueType& entry);
     double getMedian() const;
 
 private:
@@ -17,20 +18,53 @@ private:
 };
 
 template <typename ValueType>
-void Median<ValueType>::add(ValueType number)
+void Median<ValueType>::add(const ValueType& entry)
 {
+    if(leftHalf_.empty() && rightHalf_.empty())
+    {
+        leftHalf_.emplace(entry);
+    }
+    else
+    {
+        if(entry > leftHalf_.top())
+            rightHalf_.emplace(entry);
+        else
+            leftHalf_.emplace(entry);
 
+        auto moveNeeded = [](const auto& left, const auto& right)
+        {
+            return left.size() > right.size() + 1;
+        };
+
+        auto moveTop = [](auto& source, auto& destination)
+        {
+            destination.emplace(source.top());
+            source.pop();
+        };
+
+        if(moveNeeded(leftHalf_, rightHalf_))
+            moveTop(leftHalf_, rightHalf_);
+        else if(moveNeeded(rightHalf_, leftHalf_))
+            moveTop(rightHalf_, leftHalf_);
+    }
 }
 
 template <typename ValueType>
 double Median<ValueType>::getMedian() const
 {
+    if(leftHalf_.empty() && rightHalf_.empty())
+    {
+        //todo what?
+        return 0.0;
+    }
     if(leftHalf_.size() == rightHalf_.size())
     {
         return (leftHalf_.top() + rightHalf_.top()) / 2.0;
     }
-    else {
-        //todo
-        return 0.0;
+    else
+    {
+        return leftHalf_.size() > rightHalf_.size() ?
+               leftHalf_.top() :
+               rightHalf_.top();
     }
 }
--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include "Median.h"
+#include <iostream>
 
 int main()
 {
+    Median<int> m;
+    m.add(1);
+    m.add(1);
+    m.add(3);
+    //m.add(4);
+    m.add(3);
+    m.add(5);
+    //m.add(2);
+
+    std::cout << m.getMedian() << std::endl;
+
     return 0;
 }
