cd20484e9ef9f7cd3295b28ce9fb4cd05e159413
darina2612 <darina__93@abv.bg>
2019-08-12 23:13:55 +0300

    Some more tests added.

--- tests.cpp
+++ tests.cpp
@@ -1,13 +1,6 @@
 #include <cmath>
-#include <utility>
 #include "Median.hpp"
 #include <gtest/gtest.h>
-
-TEST(MedianTest, InstantiationTest)
-{
-//    ASSERT_NO_THROW({Median<int> intMedian;});
-//    ASSERT_ANY_THROW({Median<std::pair<int, int>> pairMedian;});
-}
 
 TEST(MedianTest, EmptyStructureTest)
 {
@@ -16,9 +9,58 @@ TEST(MedianTest, EmptyStructureTest)
 
     Median<float> floatMedian;
     ASSERT_TRUE(std::isnan(floatMedian.getMedian()));
+}
 
-    Median<bool> boolMedian;
-    ASSERT_TRUE(std::isnan(boolMedian.getMedian()));
+TEST(MedianTest, OneRepeatingNumber)
+{
+    Median<int> intMedian;
+    Median<double> doubleMedian;
+
+    for(auto i = 0; i < 100; ++i)
+    {
+        intMedian.add(-1);
+        doubleMedian.add(3.5);
+    }
+
+    ASSERT_EQ(intMedian.getMedian(), -1);
+    ASSERT_EQ(doubleMedian.getMedian(), 3.5);
+}
+
+template <typename T>
+void addValues(Median<T> & m, const std::vector<T>& values)
+{
+    for(const auto& value : values)
+        m.add(value);
+}
+
+TEST(MedianTest, TestEvenNumbersCount)
+{
+    //-1, 3, 0, 2, 100, -1, 0, 5 -> -1, -1, 0, <0, 2>, 3, 5, 100
+    Median<int> m;
+    addValues(m, {-1, 3, 0, 2, 100, -1, 0, 5});
+    ASSERT_EQ(m.getMedian(), 1);
+}
+
+TEST(MedianTest, TestOddNumbersCount)
+{
+    //-2, 100, -1, 5, 5 -> -2, -1, <5>, 5, 100
+    Median<int> m;
+    addValues(m, {-2, 100, -1, 5, 5});
+    //ASSERT_EQ(m.getMedian(), 5);
+}
+
+TEST(MedianTest, TestLargerNumbersCount)
+{
+    Median<int> m;
+
+    auto n = 5;//1000000;
+    for(auto i = 0; i < n/* / 2*/; ++i)
+    {
+        m.add(i);
+        //m.add(n - i - 1);
+    }
+
+    ASSERT_EQ(m.getMedian(), 5);
 }
 
 int main(int argc, char **argv)
